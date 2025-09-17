ae5e9d3c7e20e3909a09d73eb44ec3ebe932cfe9
13750075 <tommy53302172@gmail.com>
2025-09-17 11:35:42 +0800

    week02

---
+++ week02/week02-2.cpp
@@ -0,0 +1,15 @@
+///week02-2.cpp
+///用c++
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    cout<<"請輸入數字";
+    string a,ans;
+    cin>>a;
+    for(int i=a.length()-1;i>=0;i--){
+        ans+=a[i];
+    }
+    cout<<a<<ans;
+}
